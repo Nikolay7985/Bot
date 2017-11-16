2faf5a38c71c2ff7b2d3527031b716e7830f461a
Nikolay7985 <33396568+Nikolay7985@users.noreply.github.com>
2017-11-16 17:33:34 +0400

    Update run.py

--- Bot/run.py
+++ Bot/run.py
@@ -1,2 +1,15 @@
- 
-#test
+"""
+Здесь будет происходить самое главное
+"""
+from data.heroClassesData import heroClasses
+from classes.hero import Hero
+
+class Game():
+    def __init__(self):
+        """
+        а если быть совсем точным, то здесь
+        """
+        hero = Hero('Alex', 'knight')
+        print(hero)
+    
+Game()
