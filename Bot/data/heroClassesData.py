ddca8ce9f2b3998f4aa8a2fcdcbd00390935526e
Nikolay7985 <33396568+Nikolay7985@users.noreply.github.com>
2017-11-16 22:13:21 +0400

    Add new heroes class

--- Bot/data/heroClassesData.py
+++ Bot/data/heroClassesData.py
@@ -1,14 +1,31 @@
-def createClassData(hp, damage, agility, armor):
+def сharacterAttributes(hp, damage, agility, armor):
     newClass = {}
-    newClass['hp'] = hp#жизнь
-    newClass['damage'] = damage#урон
-    newClass['agility'] = agility#увертливость
-    newClass['armor'] = armor#броня
+    newClass['hp'] = hp           #жизнь
+    newClass['damage'] = damage   #урон
+    newClass['agility'] = agility #уклонение
+    newClass['mana'] = mana       #запас маны
     return newClass
 
+'''
+Base Attributes:
+hp = 80
+damage = 40
+agility = 5
+mana = 50
+'''
 heroClasses = {
-    'knight': createClassData(100, 50, 10, 60),
-    'madman': createClassData(30, 100, 50, 10),    
+    'knight': сharacterAttributes(100, 50, 10, 10),
+    'bard': сharacterAttributes(30, 100, 50, 10),
+    'mage': 'маг',
+    'thief': 'вор',
+    'archer': 'лучник',
+    'atronah': 'атронах', # обладает повышенным запасом маны, но не может её восстанавливать.
+    'alchemist': 'алхимик',
+    'potholer': 'спелеолог', # нужен в основном при походе в пещеры, может использоваться для поиска руд.
+    'nomad': 'кочевник',
+    'jaeger': 'егерь',
+    'barbarian': 'варвар',
+    'mercenary': 'наемник'    
 }
 """
 todo:
