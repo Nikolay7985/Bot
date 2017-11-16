f3c755bb059509a39da833e5829bbdbac62c8239
Nikolay7985 <33396568+Nikolay7985@users.noreply.github.com>
2017-11-16 17:34:19 +0400

    Update hero.py

--- Bot/classes/hero.py
+++ Bot/classes/hero.py
@@ -5,20 +5,18 @@ class Hero(object):
     def __init__(self, name, heroClass):
         self.name = name
         self.heroClassStr = heroClass
-        self.heroClass = heroClasses['heroClass']
+        self.heroClass = heroClasses[heroClass]
         self.hp = self.heroClass['hp']
         self.damage = self.heroClass['damage']
         self.agility = self.heroClass['agility']
         self.armor = self.heroClass['armor']
-
+    
+    def __str__(self):
+        return self.printHero()
+    
     def printHero(self):
         dictEnRus = {
             'knight': 'рыцарь',
             'madman': 'безумец'
         }
-        print('Привет, меня зовут ' + self.name + ' и я ' + dictEnRus[self.heroClassStr])
-
-
-
-hero = Hero()
-hero.printHero()
+        return 'Привет, меня зовут ' + self.name + ' и я ' + dictEnRus[self.heroClassStr] + str(self.hp)
